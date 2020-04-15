cbe2ee3d37931718f03f7f6a3ed0dd580e9fccd6
Daan Toes <daan.toes@avanade.com>
2020-04-15 14:49:01 +0200

    sdfsdf

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "azurerm_subnet" "intern" {
   name                 = "intern"
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
-  address_prefix       = "10.0.2.0/24"
+  address_prefix       = "10.0.1.0/24"
 }
 
 resource "azurerm_public_ip" "mypubliclinuxip" {
